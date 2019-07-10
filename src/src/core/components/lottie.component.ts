874b9888f1ccbcfb96e5f37f0069450da292014d
Artur Androsovych <arthurandrosovich@gmail.com>
2019-07-10 19:42:30 +0300

    feat: support Ivy (#16)

--- src/src/core/components/lottie.component.ts
+++ src/src/core/components/lottie.component.ts
@@ -9,7 +9,10 @@ import {
   ViewChild,
   Self,
   Attribute,
-  PLATFORM_ID
+  PLATFORM_ID,
+  OnChanges,
+  SimpleChanges,
+  Renderer2
 } from '@angular/core';
 
 import { BaseDirective } from '../directives/base.directive';
@@ -24,25 +27,31 @@ import { LottieEventsService } from '../services/lottie-events.service';
       [style.height.px]="height"
       style="margin: 0 auto"
       [ngStyle]="styles"
-      [ngClass]="containerClass"
     ></div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [LottieEventsService]
 })
-export class LottieComponent extends BaseDirective implements OnInit {
+export class LottieComponent extends BaseDirective implements OnChanges, OnInit {
   @ViewChild('container', { static: true })
   public container: ElementRef<HTMLElement> = null!;
 
   constructor(
     private readonly ref: ChangeDetectorRef,
     private readonly zone: NgZone,
+    private readonly renderer: Renderer2,
     @Inject(PLATFORM_ID) private readonly platformId: string,
     @Self() private readonly lottieEventsService: LottieEventsService,
     @Attribute('detach') detach: string | null
   ) {
     super();
     super.setDetach(detach);
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.containerClass) {
+      this.renderer.addClass(this.container.nativeElement, changes.containerClass.currentValue);
+    }
   }
 
   public ngOnInit(): void {

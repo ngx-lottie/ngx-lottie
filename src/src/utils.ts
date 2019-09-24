2e8e1db5f4c13085c0543c09cec479a4de515074
Artur Androsovych <arthurandrosovich@gmail.com>
2019-09-24 22:47:50 +0300

    fix: set location href for Safari/iOS (#25)

--- src/src/utils.ts
+++ src/src/utils.ts
@@ -1,5 +1,11 @@
-import { AnimationFilename } from './symbols';
+import { AnimationFilename, LottiePlayer } from './symbols';
 
 export function transformAnimationFilenameToKey(animation: AnimationFilename): string {
   return `animation-${animation.split('.json')[0]}`;
+}
+
+export function setPlayerLocationHref(player: LottiePlayer, href: string): void {
+  // This is a fix for the mask on Safari/iOS
+  // https://github.com/airbnb/lottie-web/issues/1198
+  ((player as unknown) as { setLocationHref: (href: string) => void }).setLocationHref(href);
 }

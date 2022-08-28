7faf6f87dbb32dcbe8cb94b740a1f21b54144f22
Artur Androsovych <arthurandrosovich@gmail.com>
2022-08-28 19:34:49 +0300

    feat: upgrade to Angular 14 (#145)

--- apps/demos/jest.config.ts
+++ apps/demos/jest.config.ts
@@ -1,19 +1,15 @@
-module.exports = {
-  bail: true,
-  name: 'ngx-lottie',
-  displayName: 'ngx-lottie',
+/* eslint-disable */
+export default {
+  displayName: 'demos',
   preset: '../../jest.preset.js',
-  coverageReporters: ['lcov', 'cobertura'],
-  coverageDirectory: '../../coverage/libs/ngx-lottie',
-  collectCoverageFrom: ['./src/**/!(index).ts'],
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       stringifyContentPathRegex: '\\.(html|svg)$',
-
       tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
+  coverageDirectory: '../../coverage/apps/demos',
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
